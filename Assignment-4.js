4391c27b0c52597b7bc2f4bb1375338b7d0d33cb
muhammad-tamim <mdt40234@gmail.com>
2021-08-09 17:13:38 +0600

    problem 4 solved

--- Assignment-4.js
+++ Assignment-4.js
@@ -123,3 +123,26 @@ console.log(totalCost);
 /*  ----------------------
       deliveryCost end
     ----------------------*/
+
+
+/*   Problem 4    */
+/* ----------------------------------
+       Find five letter  name start
+   ----------------------------------*/
+
+
+function perfectFriend(number) {
+    for (let friend of myFriends) {
+        if (friend.length == 5) {
+            return friend;
+        }
+
+    }
+}
+let myFriends = ["Jon", 'Tamim', "Mojdalif", "Mark", "Kasim"]
+let lergestFriend = perfectFriend(myFriends)
+console.log(lergestFriend);
+
+/* ----------------------------------
+       Find five letter  name end
+   ----------------------------------*/
--- try.js
+++ try.js
@@ -1,63 +1,56 @@
+// function myFrieands(num1) {
+//     let higestage = freandsage;
+//     for (let i = 0; i < freandsage.length; i++) {
+//         const element = freandsage[i];
+//         if (element.length > higestage.length) {
+//             higestage = element;
+//         }
+//         // higestage=element;
+//     } return higestage;
+// }
+// const frieand =  ['Tamim', "Jon", "Kasim", "Mark", "Mojdalif"];
+// let oldestFrieand = myFrieands(frieand)
+// console.log(oldestFrieand);
 
-/*     Problem 3 */
+// function perfectFriend(number) {
+//     for (let i = 0; i < number.length; i++) {
+//         const element = number[i];
+//         if (myFriends.length <= 5) {
+//             return fr
+//         }
 
-/*  ----------------------
-      deliveryCost start
-    ----------------------*/
+//     }
+// }
+// let myFriends = ['Tamim', "Jon", "Kasim", "Mark", "Mojdalif"];
+// let lergestFriend = perfectFriend(myFriends)
+// console.log(lergestFriend);
 
 
-function deliveryCost(number) {
 
-    const first100Tshirt = 100;
-    const second200Tshirt = 80;
-    const thirdRestTshirt = 50;
-    /* If input is string */
-    if (typeof number == 'string') {
-        var total = "Please, Give me number";
-        return total;
+function perfectFriend(number) {
+    for (let friend of myFriends) {
+        if (friend.length == 5) {
+            return friend;
+        }
+
     }
-
-    /* If input is negative number */
-    else if (0 > number) {
-        let total = "Sorry, your number is not valid give me positive number";
-        return total;
-    }
-
-    /* If input is positive number */
-    /* first100Tshirt */
-    else if (number <= 100) {
-        let totalCost = number * first100Tshirt;
-        return totalCost;
-    }
-    /* second200Tshirt */
-    else if (number <= 200) {
-        const firstTshart = 100 * first100Tshirt;
-        const restTshirt = number - 100;
-        const secondTshirt = restTshirt * second200Tshirt;
-        const totalCost = firstTshart + secondTshirt;
-        return totalCost;
-    }
-
-    /* If T-shart more then 200 */
-    else if (200 < number) {
-        const firstTshart = 100 * first100Tshirt;
-        const secondTshirt = 100 * second200Tshirt;
-        const restNumber = number - 200;
-        const restTshirt = restNumber * thirdRestTshirt;
-        const total = firstTshart + secondTshirt + restTshirt;
-        return total;
-    }
-    else {
-        let total = "Please, Try again "
-        return total;
-    }
-
 }
-/* This is input variable */
-let totalCost = deliveryCost(500);
-console.log(totalCost);
+let myFriends = ["Jon", 'Tamim', "Mojdalif", "Mark", "Kasim"]
+let lergestFriend = perfectFriend(myFriends)
+console.log(lergestFriend);
 
 
-/*  ----------------------
-      deliveryCost end
-    ----------------------*/
+
+// function perfectFriend(number) {
+//     let friend = 0;
+//     for (let i = 0; i < number.length; i++) {
+//         const element = number[i];
+//         // console.log(element);
+//         if (element > friend) {
+//             friend = element;
+//         } return friend;
+//     }
+// }
+// let myFriends = ['Tamim', "Jon", "Kasim", "Mark", "Mojdalif"]
+// let bestFriend = perfectFriend(myFriends);
+// console.log(bestFriend);
